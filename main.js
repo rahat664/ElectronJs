b34539a44c90938dc1fd635efde2733ed1134a34
rahat664 <rahatkabir20@gmail.com>
2022-04-05 16:29:59 +0600

    Background process off to any other application

--- main.js
+++ main.js
@@ -12,3 +12,9 @@ const createWindow = () => {
 };
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
